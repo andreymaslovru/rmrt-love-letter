34a41a5051d246a391f1be50de2ecaf52b44b5fc
Andrey Maslov <a.maslov@redmadrobot.com>
2023-02-08 13:18:20 +0700

    feat: add payloads in req

--- functions/sendForm.js
+++ functions/sendForm.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
 const handler = async (event) => {
-    console.log(event, 'event')
+    const payloads = JSON.parse(event.body)
     const url = process.env.API_URL
 
     try {
-        const { data } = await axios.post(url, event)
+        const { data } = await axios.post(url, payloads)
 
         return {
             statusCode: 200,
